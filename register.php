e382b54f8ce1ca67f7608b47f15f1bdfcfb3cdbf
Nate <nathaniel.j.marcellus@gmail.com>
2023-09-29 18:13:53 -0400

    Initial commit

---
+++ register.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration</title>
+</head>
+<body>
+    <h1>Registration Page</h1>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $servername = "localhost"; 
+        $username = "root";
+        $password = "";
+        $dbname = "fakenetwork";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // Retrieve user input and perform basic input validation
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $email = $_POST["email"];
+
+        // Hash the password for security
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        // Prepare and execute the SQL query
+        $sql = "INSERT INTO jobs_accounts (username, password, email) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("sss", $username, $hashed_password, $email);
+
+        if ($stmt->execute()) {
+            echo "Registration successful. <a href='/login.php'>Login</a>";
+        } else {
+            echo "Error: " . $sql . "<br>" . $stmt->error;
+        }
+
+        // Close the database connection
+        $stmt->close();
+        $conn->close();
+    }
+    ?>
+
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="username">Username:</label>
+        <input type="text" name="username" required><br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br>
+
+        <input type="submit" name="submit" value="Register">
+    </form>
+</body>
+</html>
